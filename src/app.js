380b2bf137cd42d37998fb797b28ed48ba686a1f
psolano <paul.solano@tecsup.edu.pe>
2026-06-21 22:57:33 -0500

    fix: se corrige la conexión a MongoDB y se mejora la configuración DNS para evitar errores de conexión

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-require('./config/db');
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// conexión a DB
+require('../config/db');
