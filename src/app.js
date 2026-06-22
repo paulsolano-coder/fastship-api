af72efcf9d8d886b713fb26ece9d3afa8b709a28
psolano <paul.solano@tecsup.edu.pe>
2026-06-22 03:16:45 -0500

    feat(app): configurar Express, conexión a MongoDB, rutas de envíos y servidor base de FastShip API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,48 @@
 require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
+// ==============================
+// CONFIGURACIÓN DE VARIABLES .env
+// ==============================
 dotenv.config();
 
-// conexión a DB
-require('../config/db');
+// ==============================
+// IMPORTAR CONEXIÓN A BD
+// ==============================
+// Esto ejecuta db.js automáticamente y conecta MongoDB
+require("../config/db");
+
+// ==============================
+// CREAR APP EXPRESS
+// ==============================
+const app = express();
+
+// ==============================
+// MIDDLEWARES
+// ==============================
+app.use(express.json());
+
+// ==============================
+// RUTAS
+// ==============================
+const envioRoutes = require("./routes/envio.route");
+
+app.use("/api", envioRoutes);
+
+// ==============================
+// RUTA BASE (PRUEBA)
+// ==============================
+app.get("/", (req, res) => {
+    res.send("🚀 FastShip API funcionando correctamente");
+});
+
+// ==============================
+// LEVANTAR SERVIDOR
+// ==============================
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
